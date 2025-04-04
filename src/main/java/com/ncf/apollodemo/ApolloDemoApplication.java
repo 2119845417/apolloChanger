02bff6651dd9e31d5c9d9bf407a531baddb54ab9
zpc <3130996969@qq.com>
2025-04-04 22:44:15 +0800

    允许Feign调用三方API

--- src/main/java/com/ncf/apollodemo/ApolloDemoApplication.java
+++ src/main/java/com/ncf/apollodemo/ApolloDemoApplication.java
@@ -5,12 +5,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @MapperScan("com.ncf.apollodemo.dao")
+@EnableFeignClients
 public class ApolloDemoApplication {
 
     public static void main(String[] args) throws Exception {
